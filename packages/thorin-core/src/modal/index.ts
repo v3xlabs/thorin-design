b21caf9dd038198c3dddcb58e7e3bbab3eeffdd4
Luc <luc@lucemans.nl>
2024-02-15 03:44:37 +0000

    Bump

--- packages/thorin-core/src/modal/index.ts
+++ packages/thorin-core/src/modal/index.ts
@@ -11,7 +11,7 @@ export class ThorinModal extends LitElement {
             display: block;
             position: fixed;
             inset: 0;
-            background: rgba(0, 0, 0, 0.5);
+            background: rgba(0, 0, 0, 0.17);
             z-index: 1000;
             display: flex;
             justify-content: center;
@@ -22,6 +22,13 @@ export class ThorinModal extends LitElement {
         :host([open]) {
             display: flex; /* Show modal when open */
         }
+        .modal-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: fixed;
+            max-width: 100%;
+        }
         .modal {
             transition: all 250ms ease-out;
             /* overflow: hidden; Smooth resizing */
@@ -30,42 +37,55 @@ export class ThorinModal extends LitElement {
             border-radius: 24px;
             max-width: 100vw;
 
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: fixed;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            max-width: 100%;
             width: auto;
             box-sizing: border-box;
             overflow: hidden;
-        }
-        @media (max-width: 768px) {
-            .modal {
-                top: auto;
-                bottom: 0;
-                transform: translate(-50%, 0%);
-                height: auto;
-                max-height: 100vh;
-                overflow-y: auto;
-            }
+
+            max-width: var(--max-width);
+            max-height: var(--max-height);
         }
         .content {
             max-height: 100vh;
             overflow: visible;
             width: fit-content;
             height: fit-content;
+            min-width: 320px; /** Modal Min Width */
+        }
+        /* Modal Breakpoint */
+        @media (max-width: 576px) {
+            .modal-container {
+                padding: var(--thorin-spacing-2);
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                transform: unset;
+                height: auto;
+                align-items: flex-end;
+            }
+            .modal {
+                /* max-height: 100vh; */
+                overflow-y: auto;
+                width: 100%;
+
+                max-width: 100%;
+                max-height: var(--max-height);
+            }
+            .content {
+                width: 100%;
+                min-width: 0;
+            }
         }
     `;
 
     render() {
         return html`
-            <div class="modal">
-                <div class="content">
-                    <slot></slot>
-                    <thorin-button width="full">Close</thorin-button>
+            <div class="modal-container">
+                <div class="modal">
+                    <div class="content">
+                        <slot></slot>
+                        <thorin-button width="full">Close</thorin-button>
+                    </div>
                 </div>
             </div>
         `;
@@ -75,13 +95,17 @@ export class ThorinModal extends LitElement {
         const modal = this.shadowRoot?.querySelector('.modal');
         const modalContent = this.shadowRoot?.querySelector('.content');
         const resizeObserver = new ResizeObserver((entries) => {
+            const padding = 16;
+
             for (const entry of entries) {
                 const { height, width } = entry.contentRect;
 
                 console.log('Resize observed', height, width);
                 modal?.setAttribute(
                     'style',
-                    `max-height: ${height + 32}px; max-width: ${width + 32}px`
+                    `--max-height: ${height + padding * 2}px; --max-width: ${
+                        width + padding * 2
+                    }px`
                 );
                 // modalContent?.setAttribute(
                 //     'style',
